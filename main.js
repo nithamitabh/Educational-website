699c01afafe012a160bee7bf84383cf9ff00728b
nithamitabh <96220322+nithamitabh@users.noreply.github.com>
2022-02-10 18:46:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// java script for toggle menu
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+    navLinks.style.right = "0";
+    console.log("hello");
+}
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
